15ab98b84b29015bc26a32344840d8b135a55bd8
jobygeorge99 <jobygeorge995@gmail.com>
2024-02-13 00:10:54 +0530

    done

--- controllers/adminRouter.js
+++ controllers/adminRouter.js
@@ -1,18 +1,34 @@
 const express = require("express")
 const studentModel = require("../models/studentModel")
 const marksModel = require("../models/marksModel")
+const bcrypt = require("bcryptjs")
 
 
 
 const router = express.Router()
 
 router.post("/add-stud",async(req,res)=>{
-    let data = req.body
-    let studentModelObj = new studentModel(data)
-    let result = await studentModelObj.save()
-    res.json({
-        "status":"success"
+
+    hashedPasswordGenerator= async(pass)=>{
+        let salt = await bcrypt.genSalt(10)
+        return bcrypt.hash(pass,salt)
+    }
+
+    let {data} = { "data":req.body}
+    let password = data.password
+
+    hashedPasswordGenerator(password).then((hashedPassword)=>{
+        console.log(hashedPassword)
+        data.password=hashedPassword
+        
+        let studentModelObj = new studentModel(data)
+        let result = studentModelObj.save()
+        res.json({
+            "status":"success"
+        })
     })
+
+    
 })
 
 router.post("/viewall-stud",async(req,res)=>{
